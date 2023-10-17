3880ba5761f2f9b1596fa2a7a7fda52dc756f343
inntri@inntrilabs.com <inntri@inntrilabs.com>
2023-10-17 18:13:55 +0530

    set current tenant to unknown

--- src/main/java/com/inntri/support/component/HeaderExtractInterceptor.java
+++ src/main/java/com/inntri/support/component/HeaderExtractInterceptor.java
@@ -1,12 +1,15 @@
 package com.inntri.support.component;
 
 import com.inntri.support.config.TenantIdentifierResolver;
+import com.inntri.support.utils.exceptions.ComplexValidationException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Objects;
 
 @Component
 public class HeaderExtractInterceptor implements HandlerInterceptor {
@@ -15,47 +18,53 @@ public class HeaderExtractInterceptor implements HandlerInterceptor {
   private Long companyId = 0L;
   private String loggedInUser = "inntri@xxxxx.com";
 
-  @Autowired private TenantIdentifierResolver tenantIdentifierResolver;
+  @Autowired private TenantIdentifierResolver currentTenantIdentifierResolverImpl;
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
       throws Exception {
-    System.out.println("request.getPathInfo : {}"+ request.getHeader("secure"));
+    String secure = request.getHeader("secure");
+    System.out.println("request.getPathInfo : {}"+ secure);
 
     //if (request.getHeader("secure") != null && request.getHeader("secure").equals("t")) {
     //if (validator.isSecured.test(request.getHeader("path"))) {
     System.out.println("userId : {}"+ request.getHeader("userId"));
+
       if (request.getHeader("userId") != null) {
         userId = Long.valueOf(request.getHeader("userId"));
       }
     System.out.println("companyId : {}"+ request.getHeader("companyId"));
+
       if (request.getHeader("companyId") != null) {
         companyId = Long.valueOf(request.getHeader("companyId"));
       }
       loggedInUser = request.getHeader("loggedInUser");
     System.out.println("loggedInUser : {}"+ loggedInUser);
+
       if (request.getHeader("companyId") != null) {
-      tenantIdentifierResolver.setCurrentTenant(request.getHeader("companyId"));
-      } /*else {
-        tenantIdentifierResolver.setCurrentTenant(String.valueOf(16));
-      }*/
-    /*System.out.println("current tenant  : {}"+ tenantIdentifierResolver.getCurrentTenant());
-    if (Objects.equals(tenantIdentifierResolver.getCurrentTenant(), "unknown")) {
-      throw new ComplexValidationException("company.invalid","Current company selection is invalid. Please contact system admin.");
-    }*/
+      currentTenantIdentifierResolverImpl.setCurrentTenant(request.getHeader("companyId"));
+      }
+
+    System.out.println("current tenant  : {}"+ currentTenantIdentifierResolverImpl.getCurrentTenant());
+      if (currentTenantIdentifierResolverImpl.getCurrentTenant() == null) {
+        currentTenantIdentifierResolverImpl.setCurrentTenant("unknown");
+      }
+    if (secure.equals("true") && Objects.equals(currentTenantIdentifierResolverImpl.getCurrentTenant(), "unknown")) {
+      throw new ComplexValidationException("company.id","Current company selection : "+ currentTenantIdentifierResolverImpl.getCurrentTenant()+ " is invalid. Please contact system admin.");
+    }
     return true;
   }
 
 
   @Override
   public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
-    tenantIdentifierResolver.setCurrentTenant(null);
+    currentTenantIdentifierResolverImpl.setCurrentTenant(null);
   }
 
 
 
   public void setCurrentTenant(String tenant) {
-    tenantIdentifierResolver.setCurrentTenant(tenant);
+    currentTenantIdentifierResolverImpl.setCurrentTenant(tenant);
   }
 
   public Long getUserId() {
@@ -82,12 +91,13 @@ public class HeaderExtractInterceptor implements HandlerInterceptor {
     this.loggedInUser = loggedInUser;
   }
 
-  public TenantIdentifierResolver getTenantIdentifierResolver() {
-    return tenantIdentifierResolver;
+
+
+  public TenantIdentifierResolver getCurrentTenantIdentifierResolverImpl() {
+    return currentTenantIdentifierResolverImpl;
   }
 
-  public void setTenantIdentifierResolver(TenantIdentifierResolver tenantIdentifierResolver) {
-    this.tenantIdentifierResolver = tenantIdentifierResolver;
+  public void setCurrentTenantIdentifierResolverImpl(TenantIdentifierResolver currentTenantIdentifierResolverImpl) {
+    this.currentTenantIdentifierResolverImpl = currentTenantIdentifierResolverImpl;
   }
-
 }
