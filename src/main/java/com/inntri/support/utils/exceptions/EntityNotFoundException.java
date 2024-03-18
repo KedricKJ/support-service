727e9a653ca3625429cb2a6c6f1a3d40d0343fe7
inntri@inntrilabs.com <inntri@inntrilabs.com>
2024-03-18 14:06:06 +0530

    entity auditor added

--- src/main/java/com/inntri/support/utils/exceptions/EntityNotFoundException.java
+++ src/main/java/com/inntri/support/utils/exceptions/EntityNotFoundException.java
@@ -1,11 +1,10 @@
 package com.inntri.support.utils.exceptions;
 
-import org.hibernate.service.spi.ServiceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class EntityNotFoundException extends ServiceException {
+
+public class EntityNotFoundException extends RuntimeException {
 
   public EntityNotFoundException(final String errorMessage, final Throwable errorObject) {
     super(errorMessage, errorObject);
--- src/main/java/com/inntri/support/utils/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/inntri/support/utils/exceptions/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.security.auth.login.CredentialExpiredException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -76,10 +78,25 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 new ExceptionResponseWrapper(ex.getMessage()), HttpStatus.NOT_FOUND);
     }
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    //@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(PermissionException.class)
     public ResponseEntity<BaseResponseWrapper> handlePermissionException(PermissionException ex,
                                                                                 WebRequest request) {
+
+        System.out.println("Permission exception occured");
+        if (ex.getValidationFailures() != null) {
+            return new ResponseEntity<>(
+                    new ValidationFailureResponseWrapper(ex.getValidationFailures()), HttpStatus.FORBIDDEN);
+        } else {
+            return new ResponseEntity<>(
+                    new ValidationFailureResponseWrapper(ex.getField(), ex.getCode()), HttpStatus.FORBIDDEN);
+        }
+    }
+
+    //@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(CredentialsExpiredException.class)
+    public ResponseEntity<BaseResponseWrapper> handleCredentialException(PermissionException ex,
+                                                                         WebRequest request) {
 
         System.out.println("Permission exception occured");
         if (ex.getValidationFailures() != null) {
